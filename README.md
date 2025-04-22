96fb9d8bca9e4afddeb64c125c7f813864c4ff6e
dwibayunugraha <bayngrh10@gmail.com>
2025-04-22 15:46:54 +0700

    P1: Jawaban Soal 3

--- README.md
+++ README.md
@@ -43,3 +43,35 @@ Menambahkan 5 warna lainnya sesuai keinginan Anda pada variabel colors
     Colors.lime,
   ];
 ```
+
+### Soal No 3
+
+Jelaskan fungsi keyword yield\* pada kode tersebut!
+
+```dart
+ yield* Stream.periodic(const Duration(seconds: 1), (int t))
+```
+
+#### Penjelasan
+
+yield\* digunakan, ia mengambil seluruh nilai dari stream yang direferensikan dan meneruskannya satu per satu ke stream yang sedang dibuat.
+
+Apa maksud isi perintah kode tersebut?
+
+```dart
+ Stream<Color> getColors() async* {
+    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+      int index = t % colors.length;
+      return colors[index];
+    });
+  }
+```
+
+#### Penjelasan
+
+Kode ini membuat sebuah stream yang:
+
+- Mengeluarkan warna dari array colors secara berurutan
+- Berganti warna setiap 1 detik
+- Setelah mencapai warna terakhir di array, kembali lagi ke - warna pertama (berputar)
+
--- lib/stream.dart
+++ lib/stream.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
- 
- class ColorStream {
-   final List<Color> colors = [
-     Colors.blueGrey,
-     Colors.amber,
-     Colors.deepPurple,
-     Colors.lightBlue,
-     Colors.teal,
-     Colors.pink,
-     Colors.red,
-     Colors.green,
-     Colors.orange,
-     Colors.lime,
-   ];
- }
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.pink,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.lime,
+  ];
+
+// Untuk menghasilkan stream yang mengeluarkan warna secara berulang setiap detik
+  Stream<Color> getColors() async* {
+    // yield* digunakan untuk mengeluarkan nilai dari stream yang diberikan
+    // Stream.periodic() menghasilkan stream yang mengeluarkan nilai berulang setiap durasi yang diberikan
+    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+      int index = t % colors.length;
+      return colors[index];
+    });
+  }
+}
